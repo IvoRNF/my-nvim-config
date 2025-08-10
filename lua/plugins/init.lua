60ac2b6fad253a86abd9aa71c1cd5c99ac3af8c1
IvoRNF <ivo.filho@omnismart.com.br>
2025-08-09 21:51:02 -0300

    added: my ~/.config/nvim config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = { "javascript", "typescript", "typescriptreact" },
+    opts = {
+
+    servers = {
+      vtsls = {
+        settings = {
+          typescript = {
+            tsserver = {
+              maxTsServerMemory = 8192 
+            },
+          },
+        },
+      },
+    },
+
+
+  }, -- Configure as needed
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "scss"
+  		},
+  	},
+  },{
+    "nvim-treesitter/nvim-treesitter-angular",
+    ft = { "html" }
+
+  }
+}
