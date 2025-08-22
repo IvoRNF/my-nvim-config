d80002305099c28cc85ecdbfc065ee8fd737fce1
IvoRNF <ivo.filho@omnismart.com.br>
2025-08-22 10:20:18 -0300

    format on save <leader>w

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,11 +6,16 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- map({"n","i"}, "<leader>qq", "<cmd>lua vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR, open = true })<cr>", { desc = "Populate Quickfix List with Diagnostics Errors" })
+map("x", "<C-c>", '"+y', { desc = "Copy to clipboard" })
 
-map('x', '<C-c>', '"+y', { desc = "Copy to clipboard" })
+map({ "n" }, "<leader>w", function()
+  vim.lsp.buf.format { async = false }
+  vim.cmd('write')
+end, { desc = "format then save" })
 
-map({'n', 'v'}, '<leader>fs', function()
- require('telescope.builtin').grep_string()
-end, { desc = 'Telescope Grep String (current word/selection)' })
+map({ "n", "v" }, "<leader>fs", function()
+  require("telescope.builtin").grep_string()
+end, { desc = "Telescope Grep String (current word/selection)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
